f9002130f89483f33541514c9cbcc1e9f64865d2
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-07-29 16:30:07 -0300

    Feat: Add basics of mutex controlling

--- drivers/mutex/gpio_mutex.c
+++ drivers/mutex/gpio_mutex.c
@@ -19,8 +19,22 @@ const struct gpio_dt_spec clock_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(mutex), clo
 const struct gpio_dt_spec data_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(mutex), data_gpios);
 const struct gpio_dt_spec reset_gpio = GPIO_DT_SPEC_GET(DT_NODELABEL(mutex), reset_gpios);
 
+
+static int mutex_data_push(const bool data);
+
+static struct self {
+    bool is_init;
+} self = {
+    .is_init = false,
+};
+
 int mutex_gpio_init(void) {
     int err;
+
+    if (self.is_init) {
+        return 0;
+    }
+
     if (!gpio_is_ready_dt(&enable_gpio)) {
         return -ENODEV;
     }
@@ -56,6 +70,40 @@ int mutex_gpio_init(void) {
     if (err < 0) {
         return err;
     }
+
+    self.is_init = true;
+
+    return 0;
+}
+
+int mutex_set(uint32_t toggle) {
+    if (!self.is_init) {
+        return -ENOEXEC;
+    }
+
+    for (int i = 0; i < CONFIG_RELAY_LEN; ++i) {
+        mutex_data_push(toggle & 1);
+        toggle >>= 1;
+    }
+
+    /* TODO: toggle enable. Should check data sheet for behaviour */
+    gpio_pin_set_dt(&enable_gpio, 0);
+    gpio_pin_set_dt(&enable_gpio, 1);
+    gpio_pin_set_dt(&enable_gpio, 0);
+
+    return 0;
+}
+
+static int mutex_data_push(const bool data) {
+    if (!self.is_init) {
+        return -ENOEXEC;
+    }
+
+    gpio_pin_set_dt(&data_gpio, data);
+
+    /* TODO: Toggle clock. Should check data sheet for behaviour */
+    gpio_pin_set_dt(&clock_gpio, 1);
+    gpio_pin_set_dt(&clock_gpio, 0);
 
     return 0;
 }
--- include/app/drivers/mutex.h
+++ include/app/drivers/mutex.h
@@ -10,4 +10,6 @@
 
 int mutex_gpio_init(void);
 
+int mutex_set(uint32_t value);
+
 #endif //APP_DRIVERS_MUTEX_H_
